fbc576aa9f426848a0957c9ca2254b32c5e9cb4d
EphraimScott <Scott@L02005021924.edu.senac.br>
2024-04-04 19:45:17 -0300

    04/04

--- questao_flet_11.py
+++ questao_flet_11.py
@@ -1,10 +1,44 @@
 import flet as ft 
 
 def main(page):
+    page.title = "MLC - Minha Lista de Compras"
 
-    produtos = ["Arroz","Feijão","Quiabo"]
-    produtos.pop(1)
-    page.controls.append(produtos)
+    produtos = []
+    
+    listing = ft.Text(value="")
+    item_de_compra = ft.TextField(label="Adicionar item")
+
+    def novo_produto(e):
+        new_produto = item_de_compra.value
+        produtos.append(new_produto)
+        listing.value = "\n".join(produtos)
+        item_de_compra.value = ""
+
+        page.update()
+
+    def delete_produto(e):
+        produtos.pop()
+        listing.value = "\n".join(produtos)
+
+        page.update()
+
+    def organizando_produtos(e):
+        produtos.sort()
+        listing.value = "\n".join(produtos)
+
+        page.update()
+
+    def reverter_produtos(e):
+        produtos.reverse()
+        listing.value = "\n".join(produtos)
+
+        page.update()
+
     page.update()
+    page.add(listing, item_de_compra, 
+             ft.ElevatedButton("Novo produto", on_click=novo_produto),
+             ft.ElevatedButton("Deletar produto", on_click=delete_produto),
+             ft.ElevatedButton("Organizar produto", on_click=organizando_produtos),
+             ft.ElevatedButton("Reverter Lista", on_click=reverter_produtos))
 
 ft.app(target=main)
